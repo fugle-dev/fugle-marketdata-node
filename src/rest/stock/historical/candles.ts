82225e7f9a414f1a6bf66e4cad03a2d675724b3b
Chun-Kai Wang <chunkai1312@gmail.com>
2024-10-05 20:20:52 +0800

    feat: add sort parameter to enable sorting for stock historical candles endpoint

--- src/rest/stock/historical/candles.ts
+++ src/rest/stock/historical/candles.ts
@@ -4,7 +4,9 @@ export interface RestStockHistoricalCandlesParams {
   symbol: string;
   from?: string;
   to?: string;
+  timeframe?: 'D' | 'W' | 'M' | '1' | '3' | '5' | '10' | '15' | '30' | '60';
   fields?: string;
+  sort?: 'asc' | 'desc';
 }
 
 export interface RestStockHistoricalCandlesResponse {
@@ -12,6 +14,7 @@ export interface RestStockHistoricalCandlesResponse {
   type: string;
   exchange: string;
   market: string;
+  timeframe: string;
   data: Array<{
     date: string;
     open: number;
